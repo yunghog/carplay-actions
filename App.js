53c65995cf87d711dc2d363b6a2502eb61330ced
Samartha <samarthahm@gmail.com>
2023-12-05 00:38:00 +0530

    remove firebase

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import firebase from 'firebase';
 import AppNavigator from "./navigator";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
-firebase.initializeApp(firebaseConfig);
-
 export default function App() {
   return <AppNavigator />;
 }
